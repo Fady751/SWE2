7481d194d7334ad67ba740a6a66b82dd5bf2ff55
Fady751 <Fady.soly55@gmail.com>
2024-11-29 01:22:45 +0200

    .

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const port = 3000;
 const host = 'localhost';
 const wss = new WebSocket.Server({ port: 8080 });
 
+// message 
 
+function fun() {
+    
+}
 
 app.listen(port, host, async(err) => {
     if(err) {
