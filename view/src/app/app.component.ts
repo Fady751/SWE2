9298e4938856a398bccae3cfec7bd7a7e4dd05e9
Fady751 <Fady.soly55@gmail.com>
2024-11-30 20:17:04 +0200

    .

--- view/src/app/app.component.ts
+++ view/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './components/signup/signup.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [NavbarComponent,RouterOutlet,SignupComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
