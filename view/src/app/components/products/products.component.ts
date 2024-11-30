b0ca6bb811e0057cef6aa8cb9683ffba9c0ab6ab
Fady751 <Fady.soly55@gmail.com>
2024-11-30 20:48:37 +0200

    .

--- view/src/app/components/products/products.component.ts
+++ view/src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { iproduct } from '../../models/iproduct';
 import { CommonModule } from '@angular/common';
 import { Icategory } from '../../models/icategory';
@@ -14,99 +14,111 @@ import { RouterLink } from '@angular/router';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
-  counter: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  categories: any[] = ['Plastic', 'Paper', 'Metal', 'Glass', 'Electronics', 'Organic Waste', 'Cloth'];
+export class ProductsComponent implements OnInit {
+
+    counter: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    categories: any[] = ['Plastic', 'Paper', 'Metal', 'Glass', 'Electronics', 'Organic Waste', 'Cloth'];
 
 
-  decreaseCounter(i: number) {
-    if(this.counter[i] > 0)
-      this.counter[i]--;
-  }
-  increaseCounter(i: number) {
-    this.counter[i]++;
-  }
+    decreaseCounter(i: number) {
+        if(this.counter[i] > 0)
+        this.counter[i]--;
+    }
+    increaseCounter(i: number) {
+        this.counter[i]++;
+    }
+    filter(name: string): void {
+        if(name === "all") {
+            this.material = [...this.products];
+            return;
+        }
+        this.material = this.products.filter((mat) => mat.categoryname === name);
+    }
 
-  products = [
+    products: any[] = [
+        {
+            "id": 1,
+            "name": "Plastic Bottle",
+            "categoryname": "Plastic",
+            "recyclable": true,
+            "url_photo": "/images/materials/plastic_bottle.jpg"
+        },
+        {
+            "id": 2,
+            "name": "Paper Bag",
+            "categoryname": "Paper",
+            "recyclable": true,
+            "url_photo": "/images/materials/paper_bag.jpg"
+        },
+        {
+            "id": 3,
+            "name": "Aluminum Can",
+            "categoryname": "Metal",
+            "recyclable": true,
+            "url_photo": "/images/materials/aluminum_can.jpg"
+        },
+        {
+            "id": 4,
+            "name": "Glass Bottle",
+            "categoryname": "Glass",
+            "recyclable": true,
+            "url_photo": "/images/materials/glass_bottle.jpg"
+        },
+        {
+            "id": 5,
+            "name": "Laptop",
+            "categoryname": "Electronics",
+            "recyclable": false,
+            "url_photo": "/images/materials/laptop.jpg"
+        },
+        {
+            "id": 6,
+            "name": "Apple Peel",
+            "categoryname": "Organic Waste",
+            "recyclable": false,
+            "url_photo": "/images/materials/apple_peel.jpg"
+        },
+        {
+            "id": 7,
+            "name": "Plastic Straw",
+            "categoryname": "Plastic",
+            "recyclable": true,
+            "url_photo": "/images/materials/plastic_straw.jpg"
+        },
+        {
+            "id": 8,
+            "name": "Charger",
+            "categoryname": "Electronics",
+            "recyclable": false,
+            "url_photo": "/images/materials/charger.jpg"
+        },
+        {
+            "id": 9,
+            "name": "Metal Spoon",
+            "categoryname": "Metal",
+            "recyclable": true,
+            "url_photo": "/images/materials/metal_spoon.jpg"
+        },
+        {
+            "id": 10,
+            "name": "Clothes",
+            "categoryname": "Cloth",
+            "recyclable": false,
+            "url_photo": "/images/materials/clothes.jpg"
+        },
     {
-        "id": 1,
-        "name": "Plastic Bottle",
-        "categoryname": "Plastic",
-        "recyclable": true,
-        "url_photo": "/images/materials/plastic_bottle.jpg"
-    },
-    {
-        "id": 2,
-        "name": "Paper Bag",
-        "categoryname": "Paper",
-        "recyclable": true,
-        "url_photo": "/images/materials/paper_bag.jpg"
-    },
-    {
-        "id": 3,
-        "name": "Aluminum Can",
-        "categoryname": "Metal",
-        "recyclable": true,
-        "url_photo": "/images/materials/aluminum_can.jpg"
-    },
-    {
-        "id": 4,
-        "name": "Glass Bottle",
-        "categoryname": "Glass",
-        "recyclable": true,
-        "url_photo": "/images/materials/glass_bottle.jpg"
-    },
-    {
-        "id": 5,
-        "name": "Laptop",
-        "categoryname": "Electronics",
-        "recyclable": false,
-        "url_photo": "/images/materials/laptop.jpg"
-    },
-    {
-        "id": 6,
-        "name": "Apple Peel",
-        "categoryname": "Organic Waste",
-        "recyclable": false,
-        "url_photo": "/images/materials/apple_peel.jpg"
-    },
-    {
-        "id": 7,
-        "name": "Plastic Straw",
-        "categoryname": "Plastic",
-        "recyclable": true,
-        "url_photo": "/images/materials/plastic_straw.jpg"
-    },
-    {
-        "id": 8,
-        "name": "Charger",
-        "categoryname": "Electronics",
-        "recyclable": false,
-        "url_photo": "/images/materials/charger.jpg"
-    },
-    {
-        "id": 9,
-        "name": "Metal Spoon",
-        "categoryname": "Metal",
-        "recyclable": true,
-        "url_photo": "/images/materials/metal_spoon.jpg"
-    },
-    {
-        "id": 10,
-        "name": "Clothes",
-        "categoryname": "Cloth",
-        "recyclable": false,
-        "url_photo": "/images/materials/clothes.jpg"
-    },
- {
-        "id": 11,
-        "name": "Glass Jar",
-        "categoryname": "Glass",
-        "recyclable": true,
-        "url_photo": "/images/materials/glass_jar.jpg"
-   }
- ];
+            "id": 11,
+            "name": "Glass Jar",
+            "categoryname": "Glass",
+            "recyclable": true,
+            "url_photo": "/images/materials/glass_jar.jpg"
+    }
+    ];
 
+    material: any[] = [...this.products];
+    ngOnInit() {
+        
+    }
 }
 
 
