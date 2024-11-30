93e1e2b9ff2509f57da2edabf5188fd64d23ef08
Fady751 <Fady.soly55@gmail.com>
2024-12-01 00:08:30 +0200

    .

--- view/src/app/components/signup/signup.component.ts
+++ view/src/app/components/signup/signup.component.ts
@@ -1,10 +1,11 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
  
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   selector: 'app-signup',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA], 
@@ -62,7 +63,7 @@ export class SignupComponent {
   
       localStorage.setItem('WSToken', responseData.token);
       this.route.navigate(['/userProfile']);
-      
+
     } catch (error) {
       console.error('Error submitting signup data:', error);
     }
