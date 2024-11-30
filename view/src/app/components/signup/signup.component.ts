bcb56a585e1e8c0ddc3cfcb5a360d2fbdc1dbbe3
Fady751 <Fady.soly55@gmail.com>
2024-11-30 23:33:52 +0200

    .

--- view/src/app/components/signup/signup.component.ts
+++ view/src/app/components/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
  
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-signup',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA], 
@@ -13,11 +14,61 @@ import { Router } from '@angular/router';
 })
 export class SignupComponent {
   constructor(private route: Router) { }
-  onSubmit(): void {
-    console.log('Form Submitted!');}
-  toggleForm(): void {
-    document.querySelector('.wrapper')?.classList.toggle('active');
+
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirm_password: string = '';
+  err: string = '';
+  setName(name: string): void {
+    this.username = name;
   }
+  setEmail(email: string): void {
+    this.email = email;
+  }
+  setPassword(password: string): void {
+    this.password = password;
+  }
+  setConfirmPassword(confirm_password: string): void {
+    this.confirm_password = confirm_password;
+  }
+
+  async onSubmit(event: Event): Promise<void> {
+    event.preventDefault();
+
+    if(this.password !== this.confirm_password) {
+      this.err = "password not match confirm password";
+      return;
+    }
+    const gender = document.getElementById('gender') as HTMLSelectElement;
+
+    const data = { name: this.username, email: this.email, password: this.password, gender: gender.value };
+    try {
+      const res = await fetch('http://localhost:3000/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      const responseData = await res.json();
+  
+      if (!res.ok) {
+        this.err = responseData.message;
+        console.error('Error submitting signup data:', res.statusText);
+        return;
+      }
+  
+      localStorage.setItem('WSToken', responseData.token);
+      this.route.navigate(['/userProfile']);
+      
+    } catch (error) {
+      console.error('Error submitting signup data:', error);
+    }
+
+  }
+
   routeLogin(): void {
     this.route.navigate(['/login']);
   }
