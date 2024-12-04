20b720e395679dbb7622676f9e4b88becb73dba5
Fady751 <Fady.soly55@gmail.com>
2024-12-04 22:57:39 +0200

    .

--- view/src/app/components/editmachine/editmachine.component.ts
+++ view/src/app/components/editmachine/editmachine.component.ts
@@ -1,6 +1,7 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editmachine',
@@ -9,7 +10,8 @@ import { GoogleMapsModule } from '@angular/google-maps';
   templateUrl: './editmachine.component.html',
   styleUrl: './editmachine.component.scss'
 })
-export class EditmachineComponent {
+export class EditmachineComponent implements OnInit {
+  constructor(private route: ActivatedRoute) { }
   center: google.maps.LatLngLiteral = { lat: 30.0444, lng: 31.2357 };
   zoom = 12;
   options: google.maps.MapOptions = {
@@ -17,20 +19,62 @@ export class EditmachineComponent {
     scrollwheel: true,
     disableDefaultUI: false,
   };
+  machine: any = {};
+  on: string = '';
+  off: string = '';
+  maintenance: string = '';
+  loc: {lat: number, lng: number} | null = null;
+  async ngOnInit() {
+    this.route.paramMap.subscribe(async(params) => {
+      this.machine = {};
+      this.machine.id = params.get('id');
+      
+      const res = await fetch('http://localhost:3000/machine?id=' + this.machine.id, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        'Authorization': `${localStorage.getItem('WSToken')}`
+        }
+      });
+
+      if(!res.ok) {
+        return;
+      }
+
+      const data = await res.json();
+
+      this.machine = data.machine;
+
+      this.selectedPlace = {lat: this.machine.latitude, lng: this.machine.longitude}; 
+
+      switch(this.machine) {
+        case 'on': {
+          this.on = 'selected';
+          break;
+        }
+        case 'off': {
+          this.off = 'selected';
+          break;
+        }
+        case 'maintenance': {
+          this.maintenance = 'selected';
+          break;
+        }
+      }
+    });
+  }
 
   selectedPlace: { lat: number; lng: number } | null = null;
 
   onMapClick(event: google.maps.MapMouseEvent) {
+    console.log(this.selectedPlace);
     if (event.latLng) {
       this.selectedPlace = {
         lat: event.latLng.lat(),
         lng: event.latLng.lng(),
       };
-      console.log('Selected Coordinates:', this.selectedPlace);
     }
   }
- 
-    constructor() { }
       
 
     onSubmit(): void {
--- view/src/app/components/home/home.component.ts
+++ view/src/app/components/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { MapComponent } from '../map/map.component';
 import { GoogleMapsModule } from '@angular/google-maps';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [GoogleMapsModule, NgIf],
+  imports: [GoogleMapsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
